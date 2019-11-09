05a206848322143c668ef2d221702aeceef205a3
Max Reinmueller <maxreinmueller@gmail.com>
2019-11-09 10:17:59 -0800

    styling page

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -5,6 +5,7 @@ import DeleteBtn from "../components/DeleteBtn";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
+import BookCard from '../components/BookCard/index'
 
 class Books extends Component {
   state = {
@@ -25,39 +26,21 @@ class Books extends Component {
     return (
       <Container fluid>
         <Row>
-          <Col size="md-6">
+          <Col size="md-12">
             <Jumbotron>
-              <h1>What Books Should I Read?</h1>
+              <h1>Google Book Search</h1>
             </Jumbotron>
             <form>
               <Input name="title" placeholder="Title (required)" />
-              <Input name="author" placeholder="Author (required)" />
-              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
               <FormBtn>Submit Book</FormBtn>
             </form>
           </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <a href={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </a>
-                    <DeleteBtn />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
+          </Row>
+          <Row>
+            <Col size="md-12">
+            <BookCard></BookCard>
+            </Col>
+          </Row>
       </Container>
     );
   }
